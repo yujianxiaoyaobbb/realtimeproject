1a75527eb1c9647528613e765393666b544a59bd
1843613553@qq.com <yujianxiaoyao>
2020-02-18 23:05:33 +0800

    spark实时项目2

--- gmall-common/src/main/java/com/atguigu/GmallConstants.java
+++ gmall-common/src/main/java/com/atguigu/GmallConstants.java
@@ -3,4 +3,5 @@ package com.atguigu;
 public class GmallConstants {
     public static final String KAFKA_TOPIC_STARTUP="gmall_startup";
     public static final String KAFKA_TOPIC_EVENT="gmall_event";
+    public static final String TEST="test";
 }
